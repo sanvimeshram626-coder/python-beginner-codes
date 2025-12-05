dfa69e5a6df55ad1c1ed39150d2badc3cdf5dfbd
sanvimeshram626-coder <sanvimeshram626@gmail.com>
2025-12-05 23:19:51 +0530

    classadd.py

---
+++ classadd.py
@@ -0,0 +1,14 @@
+CLASS ADD 2 OR 3 NUMBER
+class cal:
+  def__init__(self,a=0,b=0,c=o):
+  self.a=a
+  self.b=b
+  self.c=c
+def add 2(self):
+print(self.a+self.b)
+def add 3(self):
+print(self.a+self.b+self.c)
+
+p1=cal(1,2,3)
+p1.add2()
+p1.add3()
