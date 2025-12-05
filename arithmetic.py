ac3e17de17df40041349367446f1b869550f9e6c
sanvimeshram626-coder <sanvimeshram626@gmail.com>
2025-12-05 22:16:12 +0530

    Create arithmetic.py

---
+++ arithmetic.py
@@ -0,0 +1,12 @@
+ARITHMETIC OPERATOR
+a= 4
+b= 2
+addition= a+b
+difference= a-b
+multiplication= a*b
+division= a/b
+   print("Addition is", add)
+   print("Difference is", substract)
+   print("Multiplication is", multiply)
+   print("Division is", divide)
+        
